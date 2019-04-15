2d11fbc1e21783f5c71bfcf3c66dde01d9d4ce4d
Timothy Dillman <timothydillman@Timothys-MBP.knology.net>
2019-04-15 19:19:42 -0400

    fix undefined issue

--- src/routes/appopen/index.js
+++ src/routes/appopen/index.js
@@ -1,8 +1,10 @@
 import style from "./style";
-export default (AppOpen = props => {
+const AppOpen = props => {
   return (
     <div class={style.appopen}>
       <h1>Client should be prompted to open this via the app</h1>
     </div>
   );
-});
+};
+
+export default AppOpen;
