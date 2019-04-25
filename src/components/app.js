ca83b2c08d8f32b176d884be52646aebbda64cbf
tdillman <tdillman@taxslayer.com>
2019-04-25 13:38:29 -0400

    deploy custom index file

--- src/components/app.js
+++ src/components/app.js
@@ -7,12 +7,40 @@ import Clients from "../routes/clients";
 import Client from "../routes/client";
 import Home from "../routes/home";
 import AppOpen from "../routes/appopen";
+import Offices from "../routes/offices";
+
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       name: null
     };
+    // create blog
+    var dynamicManifest = {
+      name: `${Math.random()} Cliip`,
+      short_name: `${Math.random()} Cliip`,
+      start_url: "/",
+      display: "standalone",
+      orientation: "portrait",
+      background_color: "#fff",
+      theme_color: "#673ab8",
+      icons: [
+        {
+          src: "/assets/icons/android-chrome-192x192.png",
+          type: "image/png",
+          sizes: "192x192"
+        },
+        {
+          src: "/assets/icons/android-chrome-512x512.png",
+          type: "image/png",
+          sizes: "512x512"
+        }
+      ]
+    };
+    let strManifest = JSON.stringify(dynamicManifest);
+    let blob = new Blob([strManifest], { type: "application/json" });
+    let url = URL.createObjectURL(blob);
+    document.querySelector("#app-manifest").setAttribute("href", url);
   }
   /** Gets fired when the route changes.
    *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
@@ -36,6 +64,7 @@ export default class App extends Component {
           <AppOpen path="/appopen" />
           <Clients path="/preparer/:prepid" />
           <Client path="/client/:clientid" />
+          <Offices path="/offices/:officeid" />
         </Router>
       </div>
     );
---
+++ src/routes/offices/index.js
@@ -0,0 +1,13 @@
+import { Component } from "preact";
+import style from "./style";
+class Offices extends Component {
+  render() {
+    return (
+      <div className={style.offices}>
+        <h1>Hello Offices</h1>
+      </div>
+    );
+  }
+}
+
+export default Offices;
